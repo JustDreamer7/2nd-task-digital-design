24fb7982b0c5bbace5d9c1594875b861a8c475c8
Andrew Pochestnev <pad_zavet@mail.ru>
2022-03-08 20:23:54 +0300

    add some comments

--- getDayInfo.js
+++ getDayInfo.js
@@ -13,8 +13,8 @@ function getDayInfo(date) {
     const notStrDate = new Date(`${date.split('.')[2]}-${date.split('.')[1]}-${date.split('.')[0]}`);
     const firstDayOfMonth = new Date(notStrDate.getFullYear(), notStrDate.getMonth(), 1);
     let weekNumber = Math.ceil((notStrDate.getDate() - firstDayOfMonth.getDate() + 1) / 7)
-    console.log(firstDayOfMonth.getDay())
-    console.log(notStrDate.getDay())
+    // условие для того, чтобы считать недели с учетом дня недели начала месяца,
+    // т.е. если 1-е число — Среда, 1-я неделя, то 6-е число — Понедельник, 2-я неделя
     if (firstDayOfMonth.getDay() > notStrDate.getDay()) {
         weekNumber++
     }
